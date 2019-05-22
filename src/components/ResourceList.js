4dc28566bc8b7d6bc1f53bc113f7a09855901fd6
fieldr <fieldr92@gmail.com>
2019-05-22 16:35:33 +0100

    first commit

---
+++ src/components/ResourceList.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
+
+  const fetchResource = async resource => {
+    const response = await axios.get(
+      `http://jsonplaceholder.typicode.com/${resource}`
+    );
+    setResources(response.data);
+  };
+
+  useEffect(() => {
+    // (async resource => {
+    //   const response = await axios.get(
+    //     `http://jsonplaceholder.typicode.com/${resource}`
+    //   );
+    //   setResources(response.data);
+    // })(resource);
+    fetchResource(resource);
+  }, [resource]);
+
+  return (
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ResourceList;
